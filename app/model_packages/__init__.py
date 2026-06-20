13c59adc1a927e8f3748d5e3793204b4c8d253c2
karthiknl0 <52564507+karthiknl0@users.noreply.github.com>
2026-06-20 22:12:46 +0530

    Export LocalModel package helpers

--- app/model_packages/__init__.py
+++ app/model_packages/__init__.py
@@ -4,6 +4,8 @@ from app.model_packages.manager import (
     LocalModelPackage,
     load_package,
     list_packages,
+    package_messages,
+    package_options,
     resolve_model_or_package,
 )
 
@@ -11,5 +13,7 @@ __all__ = [
     "LocalModelPackage",
     "load_package",
     "list_packages",
+    "package_messages",
+    "package_options",
     "resolve_model_or_package",
 ]
